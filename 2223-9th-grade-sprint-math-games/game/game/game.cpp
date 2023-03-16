84bd2f4b1977b52e0c714bbfffbea3991ab02ba3
AZKazakov21 <AZKazakov21@codingburgas.bg>
2023-03-16 20:51:00 +0200

    Make child classes and constructors

--- 2223-9th-grade-sprint-math-games/game/game/game.cpp
+++ 2223-9th-grade-sprint-math-games/game/game/game.cpp
@@ -8,30 +8,38 @@
 typedef Vector2 pos;
 
 class game {
+protected:
 	const int screenWidth = 900;
 	const int screenHeight = 500; 
+	int floorHeight = 100;
 
 public: 
 	game() { // constructor 
 		InitWindow(screenWidth, screenHeight, "Game");
+	}
+};
+
+class player : game{
+public: 
+	player() {
+		pos playerPos = { screenWidth / 2, screenHeight / 2 };
 		while (!WindowShouldClose()) {
 
+
 			BeginDrawing();
-
 			ClearBackground(BLACK);
-
+			DrawCircleV(playerPos, 50, WHITE);
 			EndDrawing();
-
 		}
 		CloseWindow();
-
 	}
+
 };
 
 
 int main()
 {
 	SetTargetFPS(60);
-	game* g = new game();
+	player* p = new player();
 
 }
