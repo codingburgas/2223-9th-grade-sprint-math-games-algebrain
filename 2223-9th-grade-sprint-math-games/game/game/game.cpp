5c650ccdc8b227275827d8be7fc9a6ef72f83d9a
AZKazakov21 <AZKazakov21@codingburgas.bg>
2023-03-16 20:36:57 +0200

    Make Game Class

--- 2223-9th-grade-sprint-math-games/game/game/game.cpp
+++ 2223-9th-grade-sprint-math-games/game/game/game.cpp
@@ -1,8 +1,37 @@
 
 #include <iostream>
 #include "raylib.h"
+#include <string>
+#include <vector>
+#include <map>
+
+typedef Vector2 pos;
+
+class game {
+	const int screenWidth = 900;
+	const int screenHeight = 500; 
+
+public: 
+	game() { // constructor 
+		InitWindow(screenWidth, screenHeight, "Game");
+		while (!WindowShouldClose()) {
+
+			BeginDrawing();
+
+			ClearBackground(BLACK);
+
+			EndDrawing();
+
+		}
+		CloseWindow();
+
+	}
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	SetTargetFPS(60);
+	game* g = new game();
+
 }
