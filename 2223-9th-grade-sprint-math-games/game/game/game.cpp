b59e09b9b3761b2af86016260db092054baad0a1
AZKazakov21 <AZKazakov21@codingburgas.bg>
2023-03-21 19:34:16 +0200

    Start username selection

--- 2223-9th-grade-sprint-math-games/game/game/game.cpp
+++ 2223-9th-grade-sprint-math-games/game/game/game.cpp
@@ -9,36 +9,33 @@
 typedef Vector2 pos;
 typedef Texture2D texture;
 
+const int screenWidth = 1600;
+const int screenHeight = 800;
+
 class game {
 protected:
-	const int screenWidth = 1600;
-	const int screenHeight = 800;
-	int floorHeight = screenHeight - 100;
-
-public:
-	game() { // constructor 
-		//screenWidth = GetScreenWidth();
-		//screenHeight = GetScreenHeight();
-		InitWindow(screenWidth, screenHeight, "Game");
-	}
-};
-
-class player : game {
-	void gravity() {
-			if (playerPos.y < floorHeight) {
-				playerPos.y += 5.0f;
+	void gravity(pos &playerPos) {
+		if (playerPos.y < floorHeight) {
+			playerPos.y += 5.0f;
 		}
 	}
 
-	void moveRight() {
+	void moveRight(pos &playerPos) {
 		if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) playerPos.x += 3.0f;
 	}
 
-	void moveLeft() {
+	void moveLeft(pos &playerPos) {
 		if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) playerPos.x -= 3.0f;
 	}
 
+	int floorHeight = screenHeight - 100;
 
+public:
+};
+
+// --------------------------- PLAYER CLASS ------------------------------
+
+class player : game {
 public:
 
 	texture pSprite = LoadTexture("images/png.png");
@@ -46,13 +43,16 @@ public:
 	int lives = NULL;
 	float health;
 	float starting_health;
+	std::string username;
 
-	player(float health) {
+	player(float health, std::string username) {
 		floorHeight -= pSprite.height;
 		playerPos = { 20, float(floorHeight)};
 		this->health = health;
 		starting_health = health;
+		this->username = username;
 	}
+
 
 	void ShowLives() {
 		Image heart = LoadImage("images/heart.png");
@@ -70,26 +70,79 @@ public:
 	}
 
 	void movement() {
-		moveRight();
-		moveLeft();
-		gravity();
+		moveRight(playerPos);
+		moveLeft(playerPos);
+		gravity(playerPos);
 	}
 
 };
 
+void drawCenterLines() {
+	DrawRectangle(screenWidth / 2, 0, 1, 10000, BLACK);
+	DrawRectangle(0, screenHeight / 2, 10000, 1, BLACK);
+}
+
+void selectRectangle(bool &flag, Rectangle rec) {
+	if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+		if (CheckCollisionPointRec(GetMousePosition(), rec)) {
+			flag = true;
+		}
+	}
+}
+
+// ----------------------------------- Main ----------------------------------
 
 int main()
 {
 	SetTargetFPS(60);
-	player* p = new player(5);
+	InitWindow(screenWidth, screenHeight, "Game");
+	bool gameStart = false;
+	bool clickedOnTextbox = false;
+	const int maxNameLength = 10;
+	char usernameInput[maxNameLength + 1];
+	usernameInput[0] = '\0';
+	int letterCount = 0;
+	int key = 0;
 	while (!WindowShouldClose()) {
 
-		p->movement();
+		if (!gameStart) {
+			Rectangle textbox = { screenWidth / 2 - 300 / 2, screenHeight / 2 - 50 / 2, 300,50 };
+			selectRectangle(clickedOnTextbox, textbox);
+			if (clickedOnTextbox) {
+				key = GetKeyPressed();
 
-		BeginDrawing();
-		ClearBackground(BLACK);
-		DrawTexture(p->pSprite, p->playerPos.x, p->playerPos.y, WHITE);
-		p->showHealth();
+					if ((key >= 32) && (key <= 125) && (maxNameLength < maxNameLength))
+					{
+						usernameInput[letterCount] = char(key);
+						usernameInput[letterCount + 1] = '\0';
+						letterCount++;
+					}
+					key = GetKeyPressed();
+				if (IsKeyPressed(KEY_BACKSPACE)) {
+					letterCount--;
+					if (letterCount < 0) letterCount = 0;
+					usernameInput[letterCount] = '\0';
+				}
+			}
+
+			BeginDrawing();
+			ClearBackground(LIGHTGRAY);
+			DrawText(TextFormat("%i", key), 10, 10, 10, BLACK);
+			drawCenterLines();
+			DrawText("Input your username:", screenWidth / 2 - (MeasureText("Input your username:", 50)/2), screenHeight / 2 - 100, 50, BLACK);
+			DrawRectangleRec(textbox, GRAY);
+			if (!clickedOnTextbox) DrawRectangleLines(textbox.x, textbox.y, textbox.width, textbox.height, BLACK);
+			else DrawRectangleLines(textbox.x, textbox.y, textbox.width, textbox.height, RED);
+			DrawText(usernameInput, screenWidth / 2 - 300 / 2, screenHeight / 2 - 50 / 2, 50, WHITE);
+
+		}	
+		else {
+			player* p = new player(5, "");
+			ClearBackground(BLACK);
+			p->movement();
+			DrawTexture(p->pSprite, p->playerPos.x, p->playerPos.y, WHITE);
+			p->showHealth();
+		}
 		EndDrawing();
 	}
 	CloseWindow();
