81c30d19bf2f04c3aac31f694758db16f7c7050d
AZKazakov21 <AZKazakov21@codingburgas.bg>
2023-03-23 18:31:51 +0200

    Make difficulty chosing

--- 2223-9th-grade-sprint-math-games/game/game/game.cpp
+++ 2223-9th-grade-sprint-math-games/game/game/game.cpp
@@ -112,38 +112,57 @@ int main()
 	p->health -= 200;
 	while (!WindowShouldClose()) {
 
-		//if (!gameStart) {
-		//	Rectangle textbox = { screenWidth / 2 - 300 / 2, screenHeight / 2 - 50 / 2, 300,50 };
-		//	selectRectangle(clickedOnTextbox, textbox);
-		//	if (clickedOnTextbox) {
-		//		key = GetKeyPressed();
+		if (!gameStart) {
+			//	Rectangle textbox = { screenWidth / 2 - 300 / 2, screenHeight / 2 - 50 / 2, 300,50 };
+			//	selectRectangle(clickedOnTextbox, textbox);
+			//	if (clickedOnTextbox) {
+			//		key = GetKeyPressed();
 
-		//		if ((key >= 32) && (key <= 125) && (maxNameLength < maxNameLength))
-		//		{
-		//			usernameInput[letterCount] = char(key);
-		//			usernameInput[letterCount + 1] = '\0';
-		//			letterCount++;
-		//		}
-		//		key = GetKeyPressed();
-		//		if (IsKeyPressed(KEY_BACKSPACE)) {
-		//			letterCount--;
-		//			if (letterCount < 0) letterCount = 0;
-		//			usernameInput[letterCount] = '\0';
-		//		}
-		//	}
+			//		if ((key >= 32) && (key <= 125) && (maxNameLength < maxNameLength))
+			//		{
+			//			usernameInput[letterCount] = char(key);
+			//			usernameInput[letterCount + 1] = '\0';
+			//			letterCount++;
+			//		}
+			//		key = GetKeyPressed();
+			//		if (IsKeyPressed(KEY_BACKSPACE)) {
+			//			letterCount--;
+			//			if (letterCount < 0) letterCount = 0;
+			//			usernameInput[letterCount] = '\0';
+			//		}
+			//	}
 
-		//	BeginDrawing();
-		//	ClearBackground(LIGHTGRAY);
-		//	DrawText(TextFormat("%i", key), 10, 10, 10, BLACK);
-		//	drawCenterLines();
-		//	DrawText("Input your username:", screenWidth / 2 - (MeasureText("Input your username:", 50) / 2), screenHeight / 2 - 100, 50, BLACK);
-		//	DrawRectangleRec(textbox, GRAY);
-		//	if (!clickedOnTextbox) DrawRectangleLines(textbox.x, textbox.y, textbox.width, textbox.height, BLACK);
-		//	else DrawRectangleLines(textbox.x, textbox.y, textbox.width, textbox.height, RED);
-		//	DrawText(usernameInput, screenWidth / 2 - 300 / 2, screenHeight / 2 - 50 / 2, 50, WHITE);
+			//	BeginDrawing();
+			//	ClearBackground(LIGHTGRAY);
+			//	DrawText(TextFormat("%i", key), 10, 10, 10, BLACK);
+			//	drawCenterLines();
+			//	DrawText("Input your username:", screenWidth / 2 - (MeasureText("Input your username:", 50) / 2), screenHeight / 2 - 100, 50, BLACK);
+			//	DrawRectangleRec(textbox, GRAY);
+			//	if (!clickedOnTextbox) DrawRectangleLines(textbox.x, textbox.y, textbox.width, textbox.height, BLACK);
+			//	else DrawRectangleLines(textbox.x, textbox.y, textbox.width, textbox.height, RED);
+			//	DrawText(usernameInput, screenWidth / 2 - 300 / 2, screenHeight / 2 - 50 / 2, 50, WHITE);
 
-		//}
-		//else {
+			//}
+
+			BeginDrawing();
+			ClearBackground(LIGHTGRAY);
+			//drawCenterLines();
+			DrawText("Chose your difficulty", screenWidth / 2 - (MeasureText("Input your username:", 50) / 2), screenHeight / 2 - 100, 50, BLACK);
+			Rectangle inputDifRec = {screenWidth/2 - 400, screenHeight/2, 800, 200};
+			DrawRectangleRec(inputDifRec, BLACK);
+			std::map<std::string, Rectangle> difSelect;
+			difSelect["Easy"] = {inputDifRec.x,inputDifRec.y,inputDifRec.width/3,inputDifRec.height};
+			difSelect["Normal"] = { inputDifRec.x+inputDifRec.width/3,inputDifRec.y,inputDifRec.width / 3,inputDifRec.height};
+			difSelect["Hard"] = { inputDifRec.x + (inputDifRec.width / 3)*2 ,inputDifRec.y,inputDifRec.width / 3,inputDifRec.height };
+			for (auto itr = difSelect.begin(); itr != difSelect.end(); itr++) {
+				DrawRectangleRec(itr->second,GRAY);
+				DrawRectangleLinesEx(itr->second,4,LIGHTGRAY);
+				DrawText(itr->first.c_str(), itr->second.x + itr->second.width/2 - (MeasureText(itr->first.c_str(), 30) / 2), itr->second.y + itr->second.height/2 - 15, 30, BLACK);
+			}
+
+
+		}
+		else {
 
 		ClearBackground(BLACK);
 		p->movement();
@@ -151,7 +170,7 @@ int main()
 		p->showHealth();
 		DrawRectangle(p->playerPos.x, p->playerPos.y, 10, 10, RED);
 		DrawRectangle(p->playerPos.x+p->pSprite.width, p->playerPos.y, 10, 10, RED);
-	/*}*/
+	}
 		EndDrawing();
 	}
 	CloseWindow();
